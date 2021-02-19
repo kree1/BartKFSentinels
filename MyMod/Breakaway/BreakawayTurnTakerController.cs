1f682dada64f32f2289d5a8a84c30d60382fdc35
kree1 <myopicmastermind@gmail.com>
2021-02-19 14:44:36 -0500

    Added BreakawayTurnTakerController and opening dialogue

---
+++ MyMod/Breakaway/BreakawayTurnTakerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace BartKFSentinels.Breakaway
+{
+    public class BreakawayTurnTakerController : TurnTakerController
+    {
+        public BreakawayTurnTakerController(TurnTaker turnTaker, GameController gameController): base(turnTaker, gameController)
+        {
+
+        }
+
+        public override IEnumerator StartGame()
+        {
+            // "Put {Breakaway} into play, "Criminal Courier" side up, with 30 HP."
+            IEnumerator startingHPCoroutine = base.GameController.SetHP(base.TurnTaker.FindCard("Breakaway"), 30);
+            if (base.UseUnityCoroutines)
+            {
+                yield return this.GameController.StartCoroutine(startingHPCoroutine);
+            }
+            else
+            {
+                this.GameController.ExhaustCoroutine(startingHPCoroutine);
+            }
+            // "Put {Momentum} into play, "Under Pressure" side up."
+            // ???
+            // "Shuffle the villain deck."
+            // ???
+            yield break;
+        }
+    }
+}
