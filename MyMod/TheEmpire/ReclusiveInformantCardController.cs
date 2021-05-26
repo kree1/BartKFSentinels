7d36a592bf2a7dba46209dc098d1bcb8240ac7da
kree1 <myopicmastermind@gmail.com>
2021-05-25 20:21:41 -0400

    Fix Reclusive Informant's damage staying reduced for multiple turns

--- MyMod/TheEmpire/ReclusiveInformantCardController.cs
+++ MyMod/TheEmpire/ReclusiveInformantCardController.cs
@@ -33,7 +33,7 @@ namespace BartKFSentinels.TheEmpire
 
         private bool EndOfTurnDamageCriteria(DealDamageAction dda)
         {
-            return dda.DamageSource != null && dda.DamageSource.IsSameCard(base.Card) && dda.OriginalAmount == 2 && dda.OriginalDamageType == DamageType.Psychic && dda.CardSource.Card == base.Card && Journal.GetCardPropertiesBoolean(base.Card, DidDestroyToReduce) == true;
+            return dda.DamageSource != null && dda.DamageSource.Card != null && dda.DamageSource.IsSameCard(base.Card) && dda.OriginalAmount == 2 && dda.OriginalDamageType == DamageType.Psychic && dda.CardSource.Card == base.Card && Journal.GetCardPropertiesBoolean(base.Card, DidDestroyToReduce) == true;
         }
 
         public IEnumerator DamageWithReductionResponse(PhaseChangeAction pca)
@@ -98,6 +98,7 @@ namespace BartKFSentinels.TheEmpire
             {
                 base.GameController.ExhaustCoroutine(reduceCoroutine);
             }
+            SetCardProperty(DidDestroyToReduce, false);
             yield break;
         }
 
