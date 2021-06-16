680f17c45e3ef6d3283cf8063802ad325c056ac5
kree1 <myopicmastermind@gmail.com>
2021-06-16 01:12:17 -0400

    Added Crowding the Box and Defensive Funnel CardControllers

---
+++ MyMod/TheGoalie/CrowdingTheBoxCardController.cs
@@ -0,0 +1,57 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BartKFSentinels.TheGoalie
+{
+    public class CrowdingTheBoxCardController : TheGoalieUtilityCardController
+    {
+        public CrowdingTheBoxCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "Whenever a target deals damage to {TheGoalieCharacter}, reduce damage dealt by that target to {TheGoalieCharacter} by 1 and increase damage dealt by {TheGoalieCharacter} to that target by 1 until the end of your next turn."
+            AddTrigger<DealDamageAction>((DealDamageAction dda) => dda.Target == base.CharacterCard && dda.DamageSource != null && dda.DamageSource.IsTarget && dda.DidDealDamage, ApplyStatusesResponse, TriggerType.CreateStatusEffect, TriggerTiming.After);
+        }
+
+        public IEnumerator ApplyStatusesResponse(DealDamageAction dda)
+        {
+            // "... reduce damage dealt by that target to {TheGoalieCharacter} by 1 and increase damage dealt by {TheGoalieCharacter} to that target by 1 until the end of your next turn."
+            ReduceDamageStatusEffect reduction = new ReduceDamageStatusEffect(1);
+            reduction.SourceCriteria.IsSpecificCard = dda.DamageSource.Card;
+            reduction.TargetCriteria.IsSpecificCard = base.CharacterCard;
+            reduction.UntilCardLeavesPlay(dda.DamageSource.Card);
+            reduction.UntilCardLeavesPlay(base.CharacterCard);
+            reduction.UntilEndOfNextTurn(base.TurnTaker);
+            IncreaseDamageStatusEffect increase = new IncreaseDamageStatusEffect(1);
+            increase.SourceCriteria.IsSpecificCard = base.CharacterCard;
+            increase.TargetCriteria.IsSpecificCard = dda.DamageSource.Card;
+            increase.UntilCardLeavesPlay(dda.DamageSource.Card);
+            increase.UntilCardLeavesPlay(base.CharacterCard);
+            increase.UntilEndOfNextTurn(base.TurnTaker);
+            IEnumerator reduceCoroutine = AddStatusEffect(reduction);
+            IEnumerator increaseCoroutine = AddStatusEffect(increase);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(reduceCoroutine);
+                yield return base.GameController.StartCoroutine(increaseCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(reduceCoroutine);
+                base.GameController.ExhaustCoroutine(increaseCoroutine);
+            }
+            yield break;
+        }
+    }
+}
---
+++ MyMod/TheGoalie/DefensiveFunnelCardController.cs
@@ -0,0 +1,68 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BartKFSentinels.TheGoalie
+{
+    public class DefensiveFunnelCardController : TheGoalieUtilityCardController
+    {
+        public DefensiveFunnelCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            SpecialStringMaker.ShowSpecialString(TargetsDamagedByOtherHeroesThisRound);
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "At the start of the environment turn, {TheGoalieCharacter} may deal 2 melee damage to a target that was dealt damage by another hero this round."
+            AddStartOfTurnTrigger((TurnTaker tt) => tt.IsEnvironment, TeamUpResponse, TriggerType.DealDamage);
+        }
+
+        public IEnumerator TeamUpResponse(PhaseChangeAction pca)
+        {
+            // "If a Goalposts card is in play, increase that damage by 1."
+            ITrigger increaseResponse = AddIncreaseDamageTrigger((DealDamageAction dda) => base.GameController.FindCardsWhere(GoalpostsInPlay).Any() && dda.CardSource == GetCardSource(), (DealDamageAction dda) => 1);
+
+            // "... {TheGoalieCharacter} may deal 2 melee damage to a target that was dealt damage by another hero this round."
+            IEnumerator meleeCoroutine = base.GameController.SelectTargetsAndDealDamage(base.HeroTurnTakerController, new DamageSource(base.GameController, base.CharacterCard), 2, DamageType.Melee, 1, false, 0, additionalCriteria: DamagedByOtherHeroesThisRound, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(meleeCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(meleeCoroutine);
+            }
+
+            RemoveTrigger(increaseResponse);
+            yield break;
+        }
+
+        public bool DamagedByOtherHeroesThisRound(Card c)
+        {
+            return Journal.DealDamageEntriesThisRound().Any((DealDamageJournalEntry ddje) => ddje.SourceCard.IsHeroCharacterCard && ddje.SourceCard != base.CharacterCard && ddje.TargetCard == c);
+        }
+
+        private string TargetsDamagedByOtherHeroesThisRound()
+        {
+            string start = "Targets dealt damage by heroes other than " + base.CharacterCard.Title + " this round: ";
+            IEnumerable<Card> damagedTargets = base.GameController.FindCardsWhere((Card c) => c.IsInPlayAndHasGameText && c.IsTarget && DamagedByOtherHeroesThisRound(c), visibleToCard: GetCardSource());
+            string end = null;
+            if (damagedTargets.FirstOrDefault() == null)
+            {
+                end = "None";
+            }
+            else
+            {
+                end = string.Join(", ", damagedTargets.Select(c => c.Title).ToArray());
+            }
+            return start + end + ".";
+        }
+    }
+}
