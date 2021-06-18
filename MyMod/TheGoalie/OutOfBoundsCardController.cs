d0305a8977119bc5726a9a104608dbb017d2d1af
kree1 <myopicmastermind@gmail.com>
2021-06-17 21:31:04 -0400

    Buffed Huddle Up, Move the Goalposts, Out of Bounds, Return to Sender, & Team Spirit

--- MyMod/TheGoalie/OutOfBoundsCardController.cs
+++ MyMod/TheGoalie/OutOfBoundsCardController.cs
@@ -20,25 +20,39 @@ namespace BartKFSentinels.TheGoalie
 
         public override IEnumerator Play()
         {
-            // "Destroy all Goalposts cards in your play area."
-            IEnumerator destroyCoroutine = base.GameController.DestroyCards(base.HeroTurnTakerController, new LinqCardCriteria((Card c) => c.Location == base.TurnTaker.PlayArea && IsGoalposts(c)), cardSource: GetCardSource());
+            // "Return all Goalposts cards in your play area to your hand."
+            IEnumerator returnCoroutine = base.GameController.MoveCards(base.TurnTakerController, base.GameController.FindCardsWhere(new LinqCardCriteria((Card c) => IsGoalposts(c) && c.IsInPlayAndHasGameText && c.Location.HighestRecursiveLocation.OwnerTurnTaker == base.TurnTaker, "Goalposts cards in " + base.TurnTaker.Name + "'s play area", false, false, "Goalposts card in " + base.TurnTaker.Name + "'s play area", "Goalposts cards in " + base.TurnTaker.Name + "'s play area"), visibleToCard: GetCardSource()), (Card c) => new MoveCardDestination(base.HeroTurnTaker.Hand), responsibleTurnTaker: base.TurnTaker, cardSource: GetCardSource());
             if (base.UseUnityCoroutines)
             {
-                yield return base.GameController.StartCoroutine(destroyCoroutine);
+                yield return base.GameController.StartCoroutine(returnCoroutine);
             }
             else
             {
-                base.GameController.ExhaustCoroutine(destroyCoroutine);
+                base.GameController.ExhaustCoroutine(returnCoroutine);
             }
-            // "Draw 3 cards."
-            IEnumerator drawCoroutine = base.DrawCards(base.HeroTurnTakerController, 3, optional: false, upTo: false);
+            // "{TheGoalieCharacter} deals 1 target 3 melee damage."
+            IEnumerator damageCoroutine = base.GameController.SelectTargetsAndDealDamage(base.HeroTurnTakerController, new DamageSource(base.GameController, base.CharacterCard), 3, DamageType.Melee, 1, false, 1, cardSource: GetCardSource());
             if (base.UseUnityCoroutines)
             {
-                yield return base.GameController.StartCoroutine(drawCoroutine);
+                yield return base.GameController.StartCoroutine(damageCoroutine);
             }
             else
             {
-                base.GameController.ExhaustCoroutine(drawCoroutine);
+                base.GameController.ExhaustCoroutine(damageCoroutine);
+            }
+            // "You may play a Goalposts card or draw 3 cards."
+            List<Function> options = new List<Function>();
+            options.Add(new Function(base.HeroTurnTakerController, "Play a Goalposts card", SelectionType.PlayCard, () => SelectAndPlayCardFromHand(base.HeroTurnTakerController, cardCriteria: new LinqCardCriteria((Card c) => IsGoalposts(c), "Goalposts")), onlyDisplayIfTrue: base.HeroTurnTaker.Hand.Cards.Any((Card c) => IsGoalposts(c))));
+            options.Add(new Function(base.HeroTurnTakerController, "Draw 3 cards", SelectionType.DrawCard, () => base.DrawCards(base.HeroTurnTakerController, 3, optional: true)));
+            SelectFunctionDecision choice = new SelectFunctionDecision(base.GameController, base.HeroTurnTakerController, options, true, cardSource: GetCardSource());
+            IEnumerator playDrawCoroutine = base.GameController.SelectAndPerformFunction(choice);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(playDrawCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(playDrawCoroutine);
             }
             yield break;
         }
