8f0e66ba00aa39961508294d14f03ae1d71e88d1
kree1 <myopicmastermind@gmail.com>
2022-09-25 12:25:31 -0400

    Fixed Idle Hands not tracking past locations recursively (e.g. if the target dealt damage was next to a card that then moved)

--- MyMod/Palmreader/IdleHandsCardController.cs
+++ MyMod/Palmreader/IdleHandsCardController.cs
@@ -35,18 +35,55 @@ namespace BartKFSentinels.Palmreader
             if (firstTargeting != null && firstTargeting.Count > 0)
             {
                 Card firstTarget = firstTargeting.FirstOrDefault().SelectedCard;
+                Log.Debug("IdleHandsCardController.Play: firstTarget: " + firstTarget.ToString());
                 // Figure out where firstTarget was when it was dealt damage
                 Location playArea = firstTarget.Location;
+                Log.Debug("IdleHandsCardController.Play: firstTarget's current location: " + playArea.GetFriendlyName());
                 DealDamageJournalEntry damage = Journal.DealDamageEntriesThisTurn().Where((DealDamageJournalEntry ddje) => ddje.TargetCard == firstTarget && ddje.SourceCard == base.CharacterCard && ddje.DamageType == DamageType.Melee && ddje.CardThatCausedDamageToOccur == base.Card).LastOrDefault();
-                List<MoveCardJournalEntry> movesSince = Journal.MoveCardEntriesThisTurn().Where((MoveCardJournalEntry mcje) => mcje.Card == firstTarget && Journal.GetEntryIndex(mcje) > Journal.GetEntryIndex(damage)).ToList();
+                Log.Debug("IdleHandsCardController.Play: compiling list of MoveCardJournalEntries since damage was dealt...");
+                List<MoveCardJournalEntry> movesSince = Journal.MoveCardEntriesThisTurn().Where((MoveCardJournalEntry mcje) => mcje.Card == firstTarget && mcje.FromLocation.IsInPlay && Journal.GetEntryIndex(mcje) > Journal.GetEntryIndex(damage)).ToList();
                 if (movesSince.Any())
                 {
+                    Log.Debug("IdleHandsCardController.Play: checking movesSince...");
+                    foreach(MoveCardJournalEntry move in movesSince)
+                    {
+                        Log.Debug("IdleHandsCardController.Play: move: " + move.ToString());
+                        Log.Debug("IdleHandsCardController.Play: move.FromLocation: " + move.FromLocation.GetFriendlyName());
+                        Log.Debug("IdleHandsCardController.Play: move.ToLocation: " + move.ToLocation.GetFriendlyName());
+                        Log.Debug("IdleHandsCardController.Play: move.FromLocation.HighestRecursiveLocation: " + move.FromLocation.HighestRecursiveLocation.GetFriendlyName());
+                        Log.Debug("IdleHandsCardController.Play: move.FromLocation.HighestRecursiveLocation.IsInPlay: " + move.FromLocation.HighestRecursiveLocation.IsInPlay.ToString());
+                        Log.Debug("IdleHandsCardController.Play: NumRelaysAt(move.FromLocation.HighestRecursiveLocation): " + NumRelaysAt(move.FromLocation.HighestRecursiveLocation).ToString());
+                    }
                     MoveCardJournalEntry firstMove = movesSince.FirstOrDefault();
-                    playArea = firstMove.FromLocation;
+                    Log.Debug("IdleHandsCardController.Play: firstMove: " + firstMove.ToString());
+                    playArea = firstMove.FromLocation.HighestRecursiveLocation;
+                    // If the target was moved from a place that was next to a card or under a card, figure out where *that* card was when damage was dealt
+                    if (firstMove.FromLocation.IsNextToCard || firstMove.FromLocation.IsUnderCard)
+                    {
+                        Log.Debug("IdleHandsCardController.Play: firstTarget was moved from " + firstMove.FromLocation.GetFriendlyName() + "...");
+                        Card anchor = firstMove.FromLocation.OwnerCard;
+                        Log.Debug("IdleHandsCardController.Play: firstMove.FromLocation.OwnerCard: " + anchor.Title);
+                        List<MoveCardJournalEntry> anchorMovesSince = Journal.MoveCardEntriesThisTurn().Where((MoveCardJournalEntry mcje) => mcje.Card == anchor && mcje.FromLocation.IsInPlay && Journal.GetEntryIndex(mcje) > Journal.GetEntryIndex(damage)).ToList();
+                        if (anchorMovesSince.Any())
+                        {
+                            foreach(MoveCardJournalEntry anchorMove in anchorMovesSince)
+                            {
+                                Log.Debug("IdleHandsCardController.Play: anchorMove: " + anchorMove.ToString());
+                                Log.Debug("IdleHandsCardController.Play: anchorMove.FromLocation: " + anchorMove.FromLocation.GetFriendlyName());
+                                Log.Debug("IdleHandsCardController.Play: anchorMove.ToLocation: " + anchorMove.ToLocation.GetFriendlyName());
+                                Log.Debug("IdleHandsCardController.Play: anchorMove.FromLocation.HighestRecursiveLocation: " + anchorMove.FromLocation.HighestRecursiveLocation.GetFriendlyName());
+                                Log.Debug("IdleHandsCardController.Play: anchorMove.FromLocation.HighestRecursiveLocation.IsInPlay: " + anchorMove.FromLocation.HighestRecursiveLocation.IsInPlay.ToString());
+                                Log.Debug("IdleHandsCardController.Play: NumRelaysAt(anchorMove.FromLocation.HighestRecursiveLocation): " + NumRelaysAt(anchorMove.FromLocation.HighestRecursiveLocation).ToString());
+                            }
+                            MoveCardJournalEntry firstAnchorMove = anchorMovesSince.FirstOrDefault();
+                            Log.Debug("IdleHandsCardController.Play: firstAnchorMove: " + firstAnchorMove.ToString());
+                            playArea = firstAnchorMove.FromLocation.HighestRecursiveLocation;
+                        }
+                    }
                 }
                 if (playArea.IsInPlay && NumRelaysAt(playArea) > 0)
                 {
-                    IEnumerator projectileCoroutine = base.GameController.SelectTargetsAndDealDamage(base.HeroTurnTakerController, new DamageSource(base.GameController, base.CharacterCard), 2, DamageType.Psychic, new int?(1), false, new int?(1), isIrreducible: true, additionalCriteria: (Card c) => c.Location == playArea && c != firstTarget, cardSource: GetCardSource());
+                    IEnumerator projectileCoroutine = base.GameController.SelectTargetsAndDealDamage(base.HeroTurnTakerController, new DamageSource(base.GameController, base.CharacterCard), 2, DamageType.Psychic, new int?(1), false, new int?(1), isIrreducible: true, additionalCriteria: (Card c) => c.Location.HighestRecursiveLocation == playArea && c.IsInPlayAndHasGameText && c != firstTarget, cardSource: GetCardSource());
                     if (base.UseUnityCoroutines)
                     {
                         yield return base.GameController.StartCoroutine(projectileCoroutine);
