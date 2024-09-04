f8076c258c5e2fcf1663249fd69a2fcbc807da79
kree1 <myopicmastermind@gmail.com>
2024-09-04 11:09:41 -0400

    Added Resources folder to repo, updated postbuild.bat

--- MyMod/postbuild.bat
+++ MyMod/postbuild.bat
@@ -1,10 +1,10 @@
 copy /y "C:\Users\Kendal\source\repos\BartKFSentinels\MyMod\bin\Release\BartKFMod.dll" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod"
-copy /y "C:\Users\Kendal\source\repos\BartKFSentinels\MyMod\bin\Release\manifest.json" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod"
-copy /y "C:\Users\Kendal\source\repos\BartKFSentinels\MyMod\bin\Release\preview.png" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod"
-robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\MyMod\bin\Release\DeckBrowser" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\DeckBrowser" /e
-robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\MyMod\bin\Release\Atlas" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\Atlas" /e
-robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\MyMod\bin\Release\Cutouts" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\Cutouts" /e
-robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\MyMod\bin\Release\Endings" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\Endings" /e
-robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\MyMod\bin\Release\Fonts" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\Fonts" /e
-robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\MyMod\bin\Release\LargeCardTextures" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\LargeCardTextures" /e
+copy /y "C:\Users\Kendal\source\repos\BartKFSentinels\Resources\manifest.json" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod"
+copy /y "C:\Users\Kendal\source\repos\BartKFSentinels\Resources\preview.png" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod"
+robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\Resources\DeckBrowser" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\DeckBrowser" /e
+robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\Resources\Atlas" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\Atlas" /e
+robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\Resources\Cutouts" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\Cutouts" /e
+robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\Resources\Endings" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\Endings" /e
+robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\Resources\Fonts" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\Fonts" /e
+robocopy "C:\Users\Kendal\source\repos\BartKFSentinels\Resources\LargeCardTextures" "C:\Program Files (x86)\Steam\steamapps\common\Sentinels of the Multiverse\mods\BartKFMod\LargeCardTextures" /e
 exit 0
