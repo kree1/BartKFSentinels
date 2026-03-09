3751442b1cd57e79379c0229bec84fa2dd545324
kree1 <myopicmastermind@gmail.com>
2026-03-09 15:17:23 -0400

    Within and Without now only lets one player play a card

--- MyMod/Fracture/WithinAndWithoutCardController.cs
+++ MyMod/Fracture/WithinAndWithoutCardController.cs
@@ -19,17 +19,26 @@ namespace BartKFSentinels.Fracture
 
         public override IEnumerator Play()
         {
-            // "Each player may name a keyword other than One-Shot. Each player who does reveals cards from the top of their deck until they reveal a card with the keyword they named, puts that card into play or into their hand, then shuffles the other revealed cards into their deck."
-            IEnumerator massChooseCoroutine = base.GameController.SelectTurnTakersAndDoAction(base.HeroTurnTakerController, new LinqTurnTakerCriteria((TurnTaker tt) => IsHero(tt) && !tt.IsIncapacitatedOrOutOfGame), SelectionType.RevealCardsFromDeck, (TurnTaker tt) => NameSearchResponse(tt), requiredDecisions: 0, allowAutoDecide: true, cardSource: GetCardSource());
-            if (base.UseUnityCoroutines)
+            // "Each player may name a keyword other than One-Shot. Each player who does reveals cards from the top of their deck until they reveal a card with the keyword they named, puts that card into their hand, then shuffles the other revealed cards into their deck."
+            IEnumerator massChooseCoroutine = GameController.SelectTurnTakersAndDoAction(HeroTurnTakerController, new LinqTurnTakerCriteria((TurnTaker tt) => IsHero(tt) && !tt.IsIncapacitatedOrOutOfGame), SelectionType.RevealCardsFromDeck, (TurnTaker tt) => NameSearchResponse(tt), requiredDecisions: 0, allowAutoDecide: true, cardSource: GetCardSource());
+            if (UseUnityCoroutines)
             {
-                yield return base.GameController.StartCoroutine(massChooseCoroutine);
+                yield return GameController.StartCoroutine(massChooseCoroutine);
             }
             else
             {
-                base.GameController.ExhaustCoroutine(massChooseCoroutine);
+                GameController.ExhaustCoroutine(massChooseCoroutine);
             }
-            yield break;
+            // "One player may play a non-one-shot card."
+            IEnumerator playCoroutine = GameController.SelectTurnTakerAndDoAction(new SelectTurnTakerDecision(GameController, DecisionMaker, FindTurnTakersWhere((TurnTaker tt) => IsHero(tt) && tt.IsPlayer && !tt.IsIncapacitatedOrOutOfGame && (tt as HeroTurnTaker).Hand.Cards.Any((Card c) => !c.IsOneShot)), SelectionType.PlayCard, cardSource: GetCardSource()), (TurnTaker tt) => GameController.SelectAndPlayCardFromHand(FindTurnTakerController(tt).ToHero(), true, cardCriteria: new LinqCardCriteria((Card c) => !c.IsOneShot, "non-one-shot"), cardSource: GetCardSource()));
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(playCoroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(playCoroutine);
+            }
         }
 
         public IEnumerator NameSearchResponse(TurnTaker tt)
@@ -38,83 +47,82 @@ namespace BartKFSentinels.Fracture
             if (tt.Deck.Cards.Any())
             {
                 // "[This player] may name a keyword other than One-Shot."
-                IOrderedEnumerable<string> keywords = from s in tt.Deck.Cards.SelectMany((Card c) => base.GameController.GetAllKeywords(c)).Distinct().Where((string s) => s.ToLower() != "one-shot") orderby s select s;
+                IOrderedEnumerable<string> keywords = from s in tt.Deck.Cards.SelectMany((Card c) => GameController.GetAllKeywords(c)).Distinct().Where((string s) => s.ToLower() != "one-shot") orderby s select s;
                 keywords = keywords.Concat("Another keyword - find nothing, shuffle your deck".ToEnumerable()).OrderBy((string s) => s);
                 List<SelectWordDecision> choice = new List<SelectWordDecision>();
-                IEnumerator selectCoroutine = base.GameController.SelectWord(base.GameController.FindHeroTurnTakerController(tt.ToHero()), keywords, SelectionType.SelectKeyword, choice, true, cardSource: GetCardSource());
-                if (base.UseUnityCoroutines)
+                IEnumerator selectCoroutine = GameController.SelectWord(GameController.FindHeroTurnTakerController(tt.ToHero()), keywords, SelectionType.SelectKeyword, choice, true, cardSource: GetCardSource());
+                if (UseUnityCoroutines)
                 {
-                    yield return base.GameController.StartCoroutine(selectCoroutine);
+                    yield return GameController.StartCoroutine(selectCoroutine);
                 }
                 else
                 {
-                    base.GameController.ExhaustCoroutine(selectCoroutine);
+                    GameController.ExhaustCoroutine(selectCoroutine);
                 }
                 if (DidSelectWord(choice))
                 {
                     string chosen = GetSelectedWord(choice);
-                    // "[This player] reveals cards from the top of their deck until they reveal a card with the keyword they named, puts that card into play or into their hand, then shuffles the other revealed cards into their deck."
+                    // "[This player] reveals cards from the top of their deck until they reveal a card with the keyword they named, puts that card into their hand, then shuffles the other revealed cards into their deck."
                     List<RevealCardsAction> revealedCards = new List<RevealCardsAction>();
-                    IEnumerator revealCoroutine = base.GameController.RevealCards(base.GameController.FindTurnTakerController(tt), tt.Deck, (Card c) => c.DoKeywordsContain(chosen), 1, revealedCards, revealedCardDisplay: RevealedCardDisplay.ShowMatchingCards, cardSource: GetCardSource());
-                    if (base.UseUnityCoroutines)
+                    IEnumerator revealCoroutine = GameController.RevealCards(GameController.FindTurnTakerController(tt), tt.Deck, (Card c) => c.DoKeywordsContain(chosen), 1, revealedCards, revealedCardDisplay: RevealedCardDisplay.ShowMatchingCards, cardSource: GetCardSource());
+                    if (UseUnityCoroutines)
                     {
-                        yield return base.GameController.StartCoroutine(revealCoroutine);
+                        yield return GameController.StartCoroutine(revealCoroutine);
                     }
                     else
                     {
-                        base.GameController.ExhaustCoroutine(revealCoroutine);
+                        GameController.ExhaustCoroutine(revealCoroutine);
                     }
                     RevealCardsAction revealed = revealedCards.FirstOrDefault();
                     if (revealed != null && revealed.FoundMatchingCards)
                     {
                         Card selectedCard = revealed.MatchingCards.FirstOrDefault();
                         List<MoveCardDestination> choices = new List<MoveCardDestination>();
-                        choices.Add(new MoveCardDestination(tt.PlayArea));
                         choices.Add(new MoveCardDestination(tt.ToHero().Hand));
                         if (selectedCard != null)
                         {
-                            IEnumerator moveCoroutine = base.GameController.SelectLocationAndMoveCard(base.GameController.FindHeroTurnTakerController(tt.ToHero()), selectedCard, choices, isPutIntoPlay: true, responsibleTurnTaker: base.TurnTaker, cardSource: GetCardSource());
-                            if (base.UseUnityCoroutines)
+                            IEnumerator moveCoroutine = GameController.SelectLocationAndMoveCard(GameController.FindHeroTurnTakerController(tt.ToHero()), selectedCard, choices, isPutIntoPlay: true, responsibleTurnTaker: TurnTaker, cardSource: GetCardSource());
+                            if (UseUnityCoroutines)
                             {
-                                yield return base.GameController.StartCoroutine(moveCoroutine);
+                                yield return GameController.StartCoroutine(moveCoroutine);
                             }
                             else
                             {
-                                base.GameController.ExhaustCoroutine(moveCoroutine);
+                                GameController.ExhaustCoroutine(moveCoroutine);
                             }
                             revealed.MatchingCards.Remove(selectedCard);
                         }
-                        IEnumerator replaceCoroutine = base.GameController.MoveCards(base.GameController.FindTurnTakerController(tt), revealed.RevealedCards.Where((Card c) => c != selectedCard), tt.Deck, responsibleTurnTaker: base.TurnTaker, cardSource: GetCardSource());
-                        if (base.UseUnityCoroutines)
+                        IEnumerator replaceCoroutine = GameController.MoveCards(GameController.FindTurnTakerController(tt), revealed.RevealedCards.Where((Card c) => c != selectedCard), tt.Deck, responsibleTurnTaker: TurnTaker, cardSource: GetCardSource());
+                        if (UseUnityCoroutines)
                         {
-                            yield return base.GameController.StartCoroutine(replaceCoroutine);
+                            yield return GameController.StartCoroutine(replaceCoroutine);
                         }
                         else
                         {
-                            base.GameController.ExhaustCoroutine(replaceCoroutine);
+                            GameController.ExhaustCoroutine(replaceCoroutine);
                         }
-                        IEnumerator shuffleCoroutine = base.GameController.ShuffleLocation(tt.Deck, cardSource: GetCardSource());
-                        if (base.UseUnityCoroutines)
+                        IEnumerator shuffleCoroutine = GameController.ShuffleLocation(tt.Deck, cardSource: GetCardSource());
+                        if (UseUnityCoroutines)
                         {
-                            yield return base.GameController.StartCoroutine(shuffleCoroutine);
+                            yield return GameController.StartCoroutine(shuffleCoroutine);
                         }
                         else
                         {
-                            base.GameController.ExhaustCoroutine(shuffleCoroutine);
+                            GameController.ExhaustCoroutine(shuffleCoroutine);
                         }
                     }
                 }
             }
             else
             {
-                IEnumerator messageCoroutine = base.GameController.SendMessageAction(tt.Name + "'s deck has no cards for " + base.Card.Title + " to reveal.", Priority.Medium, GetCardSource());
-                if (base.UseUnityCoroutines)
+                IEnumerator messageCoroutine = GameController.SendMessageAction(tt.Name + "'s deck has no cards for " + base.Card.Title + " to reveal.", Priority.Medium, GetCardSource());
+                if (UseUnityCoroutines)
                 {
-                    yield return base.GameController.StartCoroutine(messageCoroutine);
+                    yield return GameController.StartCoroutine(messageCoroutine);
                 }
                 else
                 {
-                    base.GameController.ExhaustCoroutine(messageCoroutine);
+                    GameController.ExhaustCoroutine(messageCoroutine);
                 }
             }
         }
