ffee2dee1f930a96af18cc1f96c2814164f9dcdd
kree1 <myopicmastermind@gmail.com>
2026-04-27 16:49:33 -0400

    Added TurnTakerController

---
+++ MyMod/Symphony/SymphonyTurnTakerController.cs
@@ -0,0 +1,22 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BartKFSentinels.Symphony
+{
+    public class SymphonyTurnTakerController : HeroTurnTakerController
+    {
+        public SymphonyTurnTakerController(TurnTaker turnTaker, GameController gameController)
+            : base(turnTaker, gameController)
+        {
+
+        }
+
+        public override bool AlwaysAskForDraw => true;
+    }
+}
